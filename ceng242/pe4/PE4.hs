67084691669f46e7dd08c5f920d49b5f5505ef0d
Tara <ekinunver@gmail.com>
2021-05-01 13:18:54 +0300

    initial commit

---
+++ ceng242/pe4/PE4.hs
@@ -0,0 +1,89 @@
+module PE4 where
+
+import Data.Maybe -- up to you if you want to use it or not
+
+-- Generic DictTree definition with two type arguments
+data DictTree k v = Node [(k, DictTree k v)] | Leaf v deriving Show
+
+-- Lightweight Char wrapper as a 'safe' Digit type
+newtype Digit = Digit Char deriving (Show, Eq, Ord) -- derive equality and comparison too!
+
+-- Type aliases
+type DigitTree = DictTree Digit String
+type PhoneNumber = [Digit]
+
+
+---------------------------------------------------------------------------------------------
+------------------------- DO NOT CHANGE ABOVE OR FUNCTION SIGNATURES-------------------------
+--------------- DUMMY IMPLEMENTATIONS ARE GIVEN TO PROVIDE A COMPILABLE TEMPLATE ------------
+--------------------- REPLACE THEM WITH YOUR COMPILABLE IMPLEMENTATIONS ---------------------
+---------------------------------------------------------------------------------------------
+
+
+----------
+-- Part I:
+-- Some Maybe fun! Basic practice with an existing custom datatype.
+
+-- toDigit: Safely convert a character to a digit
+toDigit :: Char -> Maybe Digit
+toDigit _ = undefined
+
+-- toDigits: Safely convert a bunch of characters to a list of digits.
+--           Particularly, an empty string should fail.
+toDigits :: String -> Maybe PhoneNumber
+toDigits _ = undefined
+
+
+-----------
+-- Part II:
+-- Some phonebook business.
+
+-- numContacts: Count the number of contacts in the phonebook...
+numContacts :: DigitTree -> Int
+numContacts _ = undefined
+    
+-- getContacts: Generate the contacts and their phone numbers in order given a tree. 
+getContacts :: DigitTree -> [(PhoneNumber, String)]
+getContacts _ = undefined
+
+-- autocomplete: Create an autocomplete list of contacts given a prefix
+-- e.g. autocomplete "32" areaCodes -> 
+--      [([Digit '2'], "Adana"), ([Digit '6'], "Hatay"), ([Digit '8'], "Osmaniye")]
+autocomplete :: String -> DigitTree -> [(PhoneNumber, String)]
+autocomplete _ _ = undefined
+
+
+-----------
+-- Example Trees
+-- Two example trees to play around with, including THE exampleTree from the text. 
+-- Feel free to delete these or change their names or whatever!
+
+exampleTree :: DigitTree
+exampleTree = Node [
+    (Digit '1', Node [
+        (Digit '3', Node [
+            (Digit '7', Node [
+                (Digit '8', Leaf "Jones")])]),
+        (Digit '5', Leaf "Steele"),
+        (Digit '9', Node [
+            (Digit '1', Leaf "Marlow"),
+            (Digit '2', Node [
+                (Digit '3', Leaf "Stewart")])])]),
+    (Digit '3', Leaf "Church"),
+    (Digit '7', Node [
+        (Digit '2', Leaf "Curry"),
+        (Digit '7', Leaf "Hughes")])]
+
+areaCodes :: DigitTree
+areaCodes = Node [
+    (Digit '3', Node [
+        (Digit '1', Node [
+            (Digit '2', Leaf "Ankara")]),
+        (Digit '2', Node [
+            (Digit '2', Leaf "Adana"),
+            (Digit '6', Leaf "Hatay"),
+            (Digit '8', Leaf "Osmaniye")])]),
+    (Digit '4', Node [
+        (Digit '6', Node [
+            (Digit '6', Leaf "Artvin")])])]
+
